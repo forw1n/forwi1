99be074d641042c234fb2b56fbaabece338ea7bc
forw1n <49986723+forw1n@users.noreply.github.com>
2019-04-27 00:15:33 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -115,4 +115,4 @@ client.on('warn', e => {
 client.on('error', e => {
   console.log(chalk.bgRed(e.replace(regToken, 'that was redacted')));
 });
-client.login(process.env.BOT_TOKEN);
+client.login(ayarlar.token);
